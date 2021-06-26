87dadf3a66c08158f02c38bf26afc4cf3f3fb5bd
Marcin Borzymowski <bak7patrycja@gmail.com>
2021-06-26 08:54:40 +0200

    Refactor 8 task

--- index.js
+++ index.js
@@ -140,7 +140,6 @@ tuple_to_object.forEach(([prop,key])=> {
 //console.log(object_from_tuple)
 
 
-
 // 9. Please write a function that takes two arrays of items and returns an array of tuples made from two input arrays at the same indexes. Excessive items should be dropped.
 // example input [1,2,3], [4,5,6,7]
 // example output [[1,4], [2,5], [3,6]]
